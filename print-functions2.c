68456162cb15b2db2d419855aa861965f1a737bb
Valritehachi <136268876+Valritehachi@users.noreply.github.com>
2023-11-13 15:36:50 +0300

    Updated print-functions2.c

--- print-functions2.c
+++ print-functions2.c
@@ -2,14 +2,14 @@
 
 /****************** PRINT POINTER ******************/
 /**
- * print_pointer - Prints the value of a pointer variable
- * @types: List a of arguments
+ * print_pointer - Prints value of a pointer
+ * @types: argument list
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: get width
- * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed.
+ * @precision: Precision specification
+ * Return: chars printed.
  */
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
