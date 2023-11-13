e67e2977d4e717ec4680df4e5acdc3462b033847
Valritehachi <136268876+Valritehachi@users.noreply.github.com>
2023-11-13 15:39:42 +0300

    Updated print-functions.c

--- print-functions.c
+++ print-functions.c
@@ -4,11 +4,11 @@
 
 /**
  * print_char - Prints a char
- * @types: List a of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @width: Width
- * @precision: Precision specification
+ * @precision: Precision specifier
  * @size: Size specifier
  * Return: Number of chars printed
  */
